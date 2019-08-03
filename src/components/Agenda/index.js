c11d925bfcad526dbef464455216fe4ec718af3c
M094650 <lucas.selker@macys.com>
2019-08-03 13:11:05 -0700

    Level 3. Grouping by Department

--- src/components/Agenda/index.js
+++ src/components/Agenda/index.js
@@ -3,6 +3,7 @@
 import React, { Component } from 'react'
 import { computed, observable } from 'mobx'
 import { observer, inject } from 'mobx-react'
+import _ from 'lodash'
 
 import Greeting from 'lib/greeting'
 
@@ -11,6 +12,7 @@ import type Account from 'src/models/Account'
 import List from './List'
 import EventCell from './EventCell'
 import CalendarSelect from './CalendarSelect'
+import SectionHeader from './SectionHeader';
 
 import style from './style'
 
@@ -34,12 +36,19 @@ class Agenda extends Component<tProps> {
     id: 'All',
   };
 
+  /*
+  * Create an observable to track whether the group toggle is on or off
+  */
+ @observable groupToggle = {
+   on: false,
+ };
+
   /**
    * Return events from all calendars, sorted by date-time.
    * Returned objects contain both Event and corresponding Calendar
    */
   @computed
-  get events (): Array<{ calendar: Calendar, event: Event }> {
+  get events(): Array<{ calendar: Calendar, event: Event }> {
     const events = this.props.account.calendars.filter(c => c.id === this.selectedCalendar.id || this.selectedCalendar.id === 'All')
       .map((calendar) => (
         calendar.events.map((event) => (
@@ -60,27 +69,62 @@ class Agenda extends Component<tProps> {
       id: e.target.value,
     }
   }
+
+  // Function to handle users toggling the group functionality
+  handleGroupToggle = (e) => {
+    this.groupToggle = e.target.checked
+  }
+
+  // Use this function to get dept grouping if the toggle is on
+  renderEvents = () => {
+    const depts = _.groupBy(this.events, function (currentObject) {
+      return currentObject.event.department === undefined ? 'No Dept' : currentObject.event.department
+    })
+    let groupedResults = []
+    for (let dept in depts) {
+      groupedResults.push(
+        <div key={dept}>
+          <SectionHeader label={dept} />
+          <List>
+            {this.events.filter(e => e.event.department === dept || (e.event.department === undefined && dept === 'No Dept')).map(({ calendar, event }) => (
+              <EventCell key={event.id} calendar={calendar} event={event} />
+            ))}
+          </List>
+        </div>,
+      )
+    }   
+    if (this.groupToggle === true) {
+      return groupedResults
+    } else {
+      return <List>
+        {this.events.map(({ calendar, event }) => (
+          <EventCell key={event.id} calendar={calendar} event={event} />
+        ))}
+      </List>
+    }
+  }
+
   render () {
     return (
       <div className={style.outer}>
         <div className={style.container}>
-
-          <div className={style.header} style={{display: 'flex', justifyContent: 'space-around'}}>
+          <div className={style.header} >
             <div>
               <span className={style.title}>
                 <Greeting />
               </span>
             </div>
+          </div>
+          <div className={style.header} style={{display: 'flex', justifyContent: 'space-around'}} >
             <CalendarSelect
               calendars={this.props.account.calendars}
               handleCalendarChange={this.handleCalendarChange} />
+            <div className={style.checkboxContainer}>
+              <span>Group Results: </span>
+              <input type='checkbox' onChange={this.handleGroupToggle} />
+            </div>
           </div>
-          <List>
-            {this.events.map(({ calendar, event }) => (
-              <EventCell key={event.id} calendar={calendar} event={event} />
-            ))}
-          </List>
-
+          {this.renderEvents()}
         </div>
       </div>
     )
